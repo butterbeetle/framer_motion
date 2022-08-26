c05274d6fb8ce1b4300e7b8a2ecc61564f1a33c5
butterbeetle <butterbeetle@naver.com>
2022-08-26 23:49:15 +0900

    Gestures part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -9,34 +10,43 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255,255,255,0.5);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  background-color: rgba(255,255,255,1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants = {
-  hover: { scale: 1.5, rotate: 90 },
-  tap: { scale: 1, borderRadius: "100px" },
-  drag: {
-    backgroundColor: "rgb(46, 204, 113)",
-    transition: {
-      duration: 10,
-    }
-  }
+  hover: { rotate: 90 },
+  tap: { borderRadius: "100px" },
+
 }
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={boxVariants}
-        whileHover="hover"
-        whileDrag="drag"
-        whileTap="tap" />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileTap="tap" />
+      </BiggerBox>
     </Wrapper>
   );
 }
