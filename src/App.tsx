086ca2aabe7ed5f84db16002287499484cf8e4aa
butterbeetle <butterbeetle@naver.com>
2022-08-26 22:28:40 +0900

    Variants part One

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,18 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const variants = {
+  start: { scale: 0 },
+  end: { scale: 1, rotate: 360, transtion: { type: "spring", delay: 0.5 } }
+}
+
 function App() {
   return (
     <Wrapper>
       <Box
-        transition={{ type: "spring", delay: 0.5 }}
-        initial={{ scale: 0 }} // 초기 스타일
-        animate={{ scale: 1, rotate: 360 }} // 최종 스타일
+        variants={variants}
+        initial="start"
+        animate="end"
       />
     </Wrapper>
   );
