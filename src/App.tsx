931ffe7896b84b007813b67b13dab7ab594d12cf
butterbeetle <butterbeetle@naver.com>
2022-08-30 00:58:28 +0900

    Slider part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import { motion, AnimatePresence, useMotionValue, useScroll, useTransform } from "framer-motion";
-import { useEffect, useRef, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
@@ -8,6 +8,7 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
@@ -15,45 +16,60 @@ const Box = styled(motion.div)`
   height: 200px;
   background-color: rgba(255,255,255,1);
   border-radius: 40px;
+  display:flex;
+  justify-content:center;
+  align-items: center;
+  font-size: 25px;
   position: absolute;
   top:100px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  initial: {
+const box = {
+  invisible: {
+    x: 500,
     opacity: 0,
     scale: 0,
   },
   visible: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotate: 360,
+    transition: {
+      duration: 1,
+    }
   },
-  leaving: {
+  exit: {
+    x: -500,
     opacity: 0,
     scale: 0,
-    y: 20,
+    transition: {
+      duration: 1,
+    }
   },
 }
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => {
-    setShowing(prev => !prev);
+  const [visible, setVisible] = useState(1);
+  const nextBox = () => {
+    setVisible(prev => prev === 10 ? 1 : prev + 1);
   }
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
       <AnimatePresence> // 안쪽에 나타나거나 사라지는게 있는지 확인함
-        {showing ?
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          /> : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+          i === visible ?
+            <Box
+              variants={box}
+              initial="invisible"
+              animate="visible"
+              exit="exit"
+              key={i}>
+              {i}
+            </Box> : null
+        ))}
       </AnimatePresence>
+      <button onClick={nextBox}>NEXT</button>
     </Wrapper>
   );
 }
