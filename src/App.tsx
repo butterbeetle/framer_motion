eef753b0634a0da2b97a14642bac1f95eeb764e3
butterbeetle <butterbeetle@naver.com>
2022-08-30 02:10:05 +0900

    Final Project part Two

--- src/App.tsx
+++ src/App.tsx
@@ -38,21 +38,18 @@ const Overlay = styled(motion.div)`
 `;
 
 function App() {
-  const [clicked, setClicked] = useState(false);
-  const toggle = () => {
-    setClicked(prev => !prev);
-  }
+  const [id, setId] = useState<null | string>(null);
   return (
-    <Wrapper onClick={toggle}>
+    <Wrapper>
       <Grid>
-        <Box layoutId="hello" />
-        <Box />
-        <Box />
-        <Box />
+        {["1", "2", "3", "4"].map(n =>
+          <Box onClick={() => setId(n)} key={n} layoutId={n} />
+        )}
       </Grid>
       <AnimatePresence>
-        {clicked ?
+        {id ?
           <Overlay
+            onClick={() => setId(null)}
             initial={{
               backgroundColor: "rgba(0,0,0,0)"
             }}
@@ -63,7 +60,7 @@ function App() {
               backgroundColor: "rgba(0,0,0,0)"
             }}
           >
-            <Box layoutId="hello" style={{ width: 400, height: 200 }} />
+            <Box layoutId={id} style={{ width: 400, height: 200 }} />
           </Overlay>
           : null}
       </AnimatePresence>
